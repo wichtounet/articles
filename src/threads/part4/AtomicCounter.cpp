a2ea460672fa3657fc42e08d971df85d1468016a
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-14 12:00:49 +0200

    New article about atomics

---
+++ src/threads/part4/AtomicCounter.cpp
@@ -0,0 +1,41 @@
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <vector>
+
+struct AtomicCounter {
+    std::atomic<int> value;
+
+    void increment(){
+        ++value;
+    }
+
+    void decrement(){
+        --value;
+    }
+
+    int get(){
+        return value.load();
+    }
+};
+
+int main(){
+    AtomicCounter counter;
+
+    std::vector<std::thread> threads;
+    for(int i = 0; i < 10; ++i){
+        threads.push_back(std::thread([&counter](){
+            for(int i = 0; i < 500; ++i){
+                counter.increment();
+            }
+        }));
+    }
+
+    for(auto& thread : threads){
+        thread.join();
+    }
+
+    std::cout << counter.get() << std::endl;
+
+    return 0;
+}
