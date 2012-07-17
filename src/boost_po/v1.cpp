78faac1a3570f8defb07d8f6cef1abe775a388fc
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-17 21:29:55 +0200

    Final version of the configuration

--- src/boost_po/v1.cpp
+++ src/boost_po/v1.cpp
@@ -8,16 +8,40 @@ int main(int argc, const char* argv[]){
     po::options_description description("MyTool Usage");
 
     description.add_options()
-        ("help", "Display this help message")
-        ("version", "Display the version number");
+        ("help,h", "Display this help message")
+        ("compression,c", po::value<int>()->default_value(5)->implicit_value(10),"Compression level")
+        ("input-files", po::value<std::vector<std::string>>(), "Input files")
+        ("version,v", "Display the version number");
+
+    po::positional_options_description p;
+    p.add("input-files", -1);
 
     po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
+    po::store(po::command_line_parser(argc, argv).options(description).positional(p).run(), vm);
     po::notify(vm);
 
     if(vm.count("help")){
         std::cout << description;
+
+        return 0;
     }
+
+    if(vm.count("version")){
+        std::cout << "MyTool Version 1.0" << std::endl;
+
+        return 0;
+    }
+
+    if(vm.count("compression")){
+        std::cout << "Compression level " << vm["compression"].as<int>() << std::endl;
+    }
+
+if(vm.count("input-files")){
+    std::vector<std::string> files = vm["input-files"].as<std::vector<std::string>>();
+    for(std::string file : files){
+        std::cout << "Input file " << file << std::endl;
+    }
+}
 
     return 0;
 }
