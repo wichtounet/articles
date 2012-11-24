a3a72f86cdcae197d46d98301ebe2b4d709dca06
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-11-23 18:53:42 -0800

    Cleanup

--- src/vector_list/bench.cpp
+++ src/vector_list/bench.cpp
@@ -9,8 +9,11 @@
 //Chrono typedefs
 typedef std::chrono::high_resolution_clock Clock;
 typedef std::chrono::milliseconds milliseconds;
+typedef std::chrono::microseconds microseconds;
 
-static const std::size_t REPEAT = 1;
+static const std::size_t REPEAT = 25;
+
+namespace {
 
 struct Small {
     std::size_t a = 0;
@@ -27,6 +30,10 @@ bool operator<(const Small& s1, const Small& s2){
     return s1.a < s2.a;
 }
 
+bool operator>=(const Small& s1, const Small& s2){
+    return s1.a >= s2.a;
+}
+
 #define CREATE(name, size)\
 struct name {\
     std::size_t a = 0;\
@@ -35,7 +42,8 @@ struct name {\
     name(std::size_t a) : a(a){}\
 };\
 bool operator==(const name& s1, const name& s2){ return s1.a == s2.a; }\
-bool operator<(const name& s1, const name& s2){ return s1.a < s2.a; }
+bool operator<(const name& s1, const name& s2){ return s1.a < s2.a; }\
+bool operator>=(const name& s1, const name& s2){ return s1.a >= s2.a; }
 
 CREATE(Medium, 3) //4*8B = 32B
 CREATE(Large, 15) //16*8B = 128B
@@ -74,10 +82,37 @@ void fill_front_list(std::size_t size){
 
 template<typename T>
 void fill_front_vector(std::size_t size){
-    std::list<T> container;
+    std::vector<T> container;
 
     for(std::size_t i = 0; i < size; ++i){
         container.insert(container.begin(), {i});
+    }
+}
+
+/* Random sorted insert */
+
+template<typename Container>
+void random_sorted_insert(std::size_t size){
+    Container container;
+
+    std::mt19937 generator;
+    std::uniform_int_distribution<std::size_t> distribution(0, std::numeric_limits<std::size_t>::max() - 1);
+
+    for(std::size_t i = 0; i < size; ++i){
+        typename Container::value_type v = {distribution(generator)};
+
+        auto it = container.begin();
+        auto end = container.end();
+
+        while(it != end){
+            if(*it >= v){
+                break;
+            }
+
+            ++it;
+        }
+
+        container.insert(it, v);
     }
 }
 
@@ -85,11 +120,11 @@ void fill_front_vector(std::size_t size){
 
 template<typename Container>
 void find(std::size_t size, Container& container){
-    std::mt19937 generator;
-    std::uniform_int_distribution<std::size_t> distribution(0, size);
+    //std::mt19937 generator;
+    //std::uniform_int_distribution<std::size_t> distribution(0, size);
 
-    for(std::size_t i = 0; i < 1000; ++i){
-        typename Container::value_type v = {distribution(generator)};
+    for(std::size_t i = 0; i < size; ++i){
+        typename Container::value_type v = {i};//distribution(generator)};
         std::find(container.begin(), container.end(), v);
     }
 }
@@ -111,16 +146,11 @@ void insert(std::size_t size, Container& container){
 /* Remove */
 
 template<typename Container>
-void remove(std::size_t size, Container& container){
-    std::mt19937 generator;
-    std::uniform_int_distribution<std::size_t> distribution(0, size - 1);
-
+void remove(Container& container){
     for(std::size_t i = 0; i < 1000; ++i){
-        typename Container::value_type v = {distribution(generator)};
+        typename Container::value_type v = {i};
         auto it = std::find(container.begin(), container.end(), v);
-        if(it != container.end()){
-            container.erase(it);
-        }
+        container.erase(it);
     }
 }
 
@@ -139,8 +169,26 @@ void sort_list(std::list<T>& container){
 /* Bench functions */
 
 template<typename Function>
+void bench_small(Function function, const std::string& type){
+    std::vector<std::size_t> sizes = {100, 1000, 10000, 50000, 100000, 200000, 300000};
+    for(auto size : sizes){
+        Clock::time_point t0 = Clock::now();
+
+        //Repeat twice
+        for(std::size_t i = 0; i < 2; ++i){
+            function(size);
+        }
+
+        Clock::time_point t1 = Clock::now();
+        milliseconds ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
+
+        std::cout << type << ":" << size << ":" << (ms.count() / 2) << "ms" << std::endl;
+    }
+}
+
+template<typename Function>
 void bench(Function function, const std::string& type){
-    std::vector<std::size_t> sizes = {1000, 10000, 100000, 1000000/*, 10000000*/};
+    std::vector<std::size_t> sizes = {1000, 10000, 100000, 1000000};
     for(auto size : sizes){
         Clock::time_point t0 = Clock::now();
 
@@ -156,13 +204,16 @@ void bench(Function function, const std::string& type){
 }
 
 template<typename Container, typename Function>
-void bench_pre(Function function, const std::string& type){
-    std::vector<std::size_t> sizes = {1000, 10000, 100000, 1000000/*, 10000000*/};
+void bench_find(Function function, const std::string& type){
+    std::vector<std::size_t> sizes = {100, 1000, 5000, 10000, 20000};
     for(auto size : sizes){
         Container container;
+        
+        std::mt19937 generator;
+        std::uniform_int_distribution<std::size_t> distribution(0, size - 1);
 
         for(std::size_t i = 0; i < size; ++i){
-            container.push_back({i});
+            container.push_back({distribution(generator)});
         }
 
         Clock::time_point t0 = Clock::now();
@@ -172,9 +223,67 @@ void bench_pre(Function function, const std::string& type){
         }
 
         Clock::time_point t1 = Clock::now();
-        milliseconds ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
+        auto d = std::chrono::duration_cast<microseconds>(t1 - t0);
 
-        std::cout << type << ":" << size << ":" << ms.count() << "ms" << std::endl;
+        std::cout << type << ":" << size << ":" << (d.count() /REPEAT) << "us" << std::endl;
+    }
+}
+
+template<typename Container, typename Function>
+void bench_insert(Function function, const std::string& type){
+    std::vector<std::size_t> sizes = {1000, 2000, 4000, 6000, 8000, 10000};
+    for(auto size : sizes){
+        std::vector<typename Container::value_type> temp;
+
+        for(std::size_t i = 0; i < size; ++i){
+            temp.push_back({i});
+        }
+
+        std::random_shuffle(temp.begin(), temp.end());
+
+        Container container(temp.begin(), temp.end());
+
+        Clock::time_point t0 = Clock::now();
+
+        for(std::size_t i = 0; i < REPEAT; ++i){
+            function(size, container);
+        }
+
+        Clock::time_point t1 = Clock::now();
+        auto d = std::chrono::duration_cast<milliseconds>(t1 - t0);
+
+        std::cout << type << ":" << size << ":" << (d.count() /REPEAT) << "ms" << std::endl;
+    }
+}
+
+template<typename Container, typename Function>
+void bench_remove(Function function, const std::string& type){
+    std::vector<std::size_t> sizes = {1000, 2000, 4000, 6000, 8000, 10000};
+    for(auto size : sizes){
+        std::vector<typename Container::value_type> temp;
+
+        for(std::size_t i = 0; i < size; ++i){
+            temp.push_back({i});
+        }
+
+        std::random_shuffle(temp.begin(), temp.end());
+
+        milliseconds ms_tot;
+
+        Container container;
+
+        for(std::size_t i = 0; i < REPEAT; ++i){
+            container.clear();
+            container.insert(container.begin(), temp.begin(), temp.end());
+
+            Clock::time_point t0 = Clock::now();
+            function(container);
+            Clock::time_point t1 = Clock::now();
+            milliseconds ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
+            ms_tot += ms;
+        }
+
+        std::cout << type << ":" << size << ":" << ms_tot.count() << "ms" << std::endl;
     }
 }
 
@@ -182,7 +291,7 @@ template<typename Container, typename Function>
 void bench_sort(Function function, const std::string& type){
     std::vector<std::size_t> sizes = {1000, 10000, 100000, 1000000/*, 10000000*/};
     for(auto size : sizes){
-        std::vector<typename Container::value_type> temp(size);
+        std::vector<typename Container::value_type> temp;
 
         for(std::size_t i = 0; i < size; ++i){
             temp.push_back({i});
@@ -219,31 +328,37 @@ void bench(){
     bench(fill_back<std::list<T>>, "list");
     
     std::cout << "Fill front" << std::endl;
-    bench(fill_front_vector<T>, "vector");
-    bench(fill_front_list<T>, "list");
+    bench_small(fill_front_vector<T>, "vector");
+    bench_small(fill_front_list<T>, "list");
     
     std::cout << "Find all" << std::endl;
-    bench_pre<std::vector<T>>(find<std::vector<T>>, "vector");
-    bench_pre<std::list<T>>(find<std::list<T>>, "list");
+    bench_find<std::vector<T>>(find<std::vector<T>>, "vector");
+    bench_find<std::list<T>>(find<std::list<T>>, "list");
     
     std::cout << "Insert" << std::endl;
-    bench_pre<std::vector<T>>(insert<std::vector<T>>, "vector");
-    bench_pre<std::list<T>>(insert<std::list<T>>, "list");
+    bench_insert<std::vector<T>>(insert<std::vector<T>>, "vector");
+    bench_insert<std::list<T>>(insert<std::list<T>>, "list");
     
     std::cout << "Remove" << std::endl;
-    bench_pre<std::vector<T>>(remove<std::vector<T>>, "vector");
-    bench_pre<std::list<T>>(remove<std::list<T>>, "list");
+    bench_remove<std::vector<T>>(remove<std::vector<T>>, "vector");
+    bench_remove<std::list<T>>(remove<std::list<T>>, "list");
     
     std::cout << "Sort" << std::endl;
     bench_sort<std::vector<T>>(sort_vector<T>, "vector");
     bench_sort<std::list<T>>(sort_list<T>, "list");
 }
 
+} //end of anonymous namespace
+
 int main(){
     bench<Small>();
     bench<Medium>();
     bench<Large>();
     bench<Huge>();
+
+    std::cout << "Random Sorted Insert" << std::endl;
+    bench_small(random_sorted_insert<std::vector<Small>>, "vector");
+    bench_small(random_sorted_insert<std::list<Small>>, "list");
 
     return 0;
 }
