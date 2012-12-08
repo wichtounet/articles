9205ab3e791ea329c292ab0c2a7e5932c9bdd43b
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-12-07 21:32:39 -0800

    Add support for new benchmarks

--- src/intrusive_list/bench.cpp
+++ src/intrusive_list/bench.cpp
@@ -20,53 +20,9 @@ static const std::size_t REPEAT = 7;
 
 namespace {
 
-struct NonTrivial {
-    std::size_t a = 0;
-    std::size_t b = 0;
-
-    NonTrivial(){}
-    NonTrivial(std::size_t a) : a(a) {}
-
-    NonTrivial& operator=(const NonTrivial& rhs){
-        double x = sqrt(b * (rhs.a / 3.14 + 73.44 * b / a) / a);
-        double y = sqrt(a * (rhs.b / 73.2198 * rhs.b * rhs.a) / b);
-
-        a = sqrt((x * tanh(x) * log10(x)) / y);
-        b = sqrt((y * y * cos(y) * y) / (sin(x) * x));
-
-        a = rhs.a;
-        b = rhs.b;
-
-        return *this;
-    }
-
-    NonTrivial(const NonTrivial& rhs){
-        double x = sqrt(b * (rhs.a / 3.14 + 73.44 * b / a) / a);
-        double y = sqrt(a * (rhs.b / 73.2198 * rhs.b * rhs.a) / b);
-
-        a = sqrt((x * tanh(x) * log10(x)) / y);
-        b = sqrt((y * y * cos(y) * y) / (sin(x) * x));
-
-        a = rhs.a;
-        b = rhs.b;
-    }
-};
-
-bool operator==(const NonTrivial& s1, const NonTrivial& s2){
-    return s1.a == s2.a;
-}
-
-bool operator<(const NonTrivial& s1, const NonTrivial& s2){
-    return s1.a < s2.a;
-}
-
-bool operator>=(const NonTrivial& s1, const NonTrivial& s2){
-    return s1.a >= s2.a;
-}
-
 template<unsigned int Size> 
 struct filler {
-    int dummy[Size];
+    std::size_t dummy[Size];
 };
 
 template <>
@@ -83,6 +39,8 @@ struct Normal : private filler<Size> {
 
     friend bool operator==(const Normal<Size>& s1, const Normal<Size>& s2){ return s1.a == s2.a; }
     friend bool operator<(const Normal<Size>& s1, const Normal<Size>& s2){ return s1.a < s2.a; }
+    friend bool operator<=(const Normal<Size>& s1, const Normal<Size>& s2){ return s1.a <= s2.a; }
+    friend bool operator>(const Normal<Size>& s1, const Normal<Size>& s2){ return s1.a > s2.a; }
     friend bool operator>=(const Normal<Size>& s1, const Normal<Size>& s2){ return s1.a >= s2.a; }
 };
 
@@ -147,52 +105,6 @@ void bench_fill_back_intrusive(const std::string& type){
         }
 
         graphs::new_result(type, std::to_string(size), us_tot.count() / REPEAT);
-    }
-}
-
-/* Fill front  */
-
-template<typename T>
-inline void fill_front_list(std::size_t size, std::list<T>& container){
-    for(std::size_t i = 0; i < size; ++i){
-        container.push_front({i});
-    }
-}
-
-template<typename T>
-inline void fill_front_deque(std::size_t size, std::deque<T>& container){
-    for(std::size_t i = 0; i < size; ++i){
-        container.push_front({i});
-    }
-}
-
-template<typename T>
-inline void fill_front_vector(std::size_t size, std::vector<T>& container){
-    for(std::size_t i = 0; i < size; ++i){
-        container.insert(container.begin(), {i});
-    }
-}
-
-template<typename Container, typename Function>
-void bench_fill_front(Function function, const std::string& type){
-    std::vector<std::size_t> sizes = {10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
-    for(auto size : sizes){
-        Container container;
-
-        milliseconds ms_tot;
-
-        //Repeat twice
-        for(std::size_t i = 0; i < 2; ++i){
-            Clock::time_point t0 = Clock::now();
-            function(size, container);
-            Clock::time_point t1 = Clock::now();
-            milliseconds ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
-            ms_tot += ms;
-
-            container.clear();
-        }
-
-        graphs::new_result(type, std::to_string(size), ms_tot.count() / 2);
     }
 }
 
@@ -366,25 +278,10 @@ void bench_remove(const std::string& type){
     }
 }
 
-/* Sort */
+/* Reverse */
 
-template<typename T>
-inline void sort_vector(std::vector<T>& container){
-    std::sort(container.begin(), container.end());
-}
-
-template<typename T>
-inline void sort_deque(std::deque<T>& container){
-    std::sort(container.begin(), container.end());
-}
-
-template<typename T>
-inline void sort_list(std::list<T>& container){
-    container.sort();
-}
-
-template<typename Container, typename Function>
-void bench_sort(Function function, const std::string& type){
+template<typename Container>
+void bench_reverse(const std::string& type){
     std::vector<std::size_t> sizes = {100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000};
     for(auto size : sizes){
         std::vector<typename Container::value_type> temp;
@@ -395,18 +292,97 @@ void bench_sort(Function function, const std::string& type){
 
         std::random_shuffle(temp.begin(), temp.end());
 
-        milliseconds ms_tot;
+        microseconds ms_tot;
 
         Container container;
+        for(std::size_t j = 0; j < temp.size(); ++j){
+            container.push_back(temp[j]);
+        }
 
         for(std::size_t i = 0; i < REPEAT; ++i){
-            container.clear();
-            container.insert(container.begin(), temp.begin(), temp.end());
-
             Clock::time_point t0 = Clock::now();
-            function(container);
+            container.reverse();
             Clock::time_point t1 = Clock::now();
-            auto ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
+            auto ms = std::chrono::duration_cast<microseconds>(t1 - t0);
+            ms_tot += ms;
+        }
+
+        graphs::new_result(type, std::to_string(size), ms_tot.count() / REPEAT);
+    }
+}
+
+/* Write */
+
+template<typename Container>
+void bench_write(const std::string& type){
+    std::vector<std::size_t> sizes = {100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000};
+    for(auto size : sizes){
+        std::vector<typename Container::value_type> temp;
+
+        for(std::size_t i = 0; i < size; ++i){
+            temp.push_back({i});
+        }
+
+        std::random_shuffle(temp.begin(), temp.end());
+
+        microseconds ms_tot;
+
+        Container container;
+        for(std::size_t j = 0; j < temp.size(); ++j){
+            container.push_back(temp[j]);
+        }
+
+        for(std::size_t i = 0; i < REPEAT; ++i){
+            Clock::time_point t0 = Clock::now();
+            
+            auto it = container.begin();
+            auto end = container.end();
+
+            for(; it != end; ++it){
+                ++(it->a);
+            }
+
+            Clock::time_point t1 = Clock::now();
+            auto ms = std::chrono::duration_cast<microseconds>(t1 - t0);
+            ms_tot += ms;
+        }
+
+        graphs::new_result(type, std::to_string(size), ms_tot.count() / REPEAT);
+    }
+}
+
+/* Sort */
+
+template<typename Container>
+void bench_sort(const std::string& type){
+    std::vector<std::size_t> sizes = {100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000};
+    for(auto size : sizes){
+        std::vector<typename Container::value_type> temp;
+
+        for(std::size_t i = 0; i < size; ++i){
+            temp.push_back({i});
+        }
+
+        std::random_shuffle(temp.begin(), temp.end());
+
+        microseconds ms_tot;
+
+        Container container;
+        for(std::size_t j = 0; j < temp.size(); ++j){
+            container.push_back(temp[j]);
+        }
+
+        for(std::size_t i = 0; i < REPEAT; ++i){
+            Clock::time_point t0 = Clock::now();
+
+            if(!(i % 2)){
+                container.sort(std::greater<typename Container::value_type>());
+            } else {
+                container.sort(std::less<typename Container::value_type>());
+            }
+            
+            Clock::time_point t1 = Clock::now();
+            auto ms = std::chrono::duration_cast<microseconds>(t1 - t0);
             ms_tot += ms;
         }
 
@@ -452,47 +428,53 @@ void bench(){
 
     std::string size_str = std::to_string(sizeof(T));
 
-    graphs::new_graph("fill_back_" + size_str, "fill_back - "  + size_str + " byte", "us");
+/*    graphs::new_graph("fill_back_" + size_str, "fill_back - "  + size_str + " byte", "us");
 
     bench_fill_back_standard<std::list<T>>("list");
     bench_fill_back_intrusive<boost::intrusive::list<I1, boost::intrusive::constant_time_size<false>>>("normal_ilist");
     bench_fill_back_intrusive<boost::intrusive::list<I2, boost::intrusive::constant_time_size<false>>>("safe_ilist");
-    bench_fill_back_intrusive<boost::intrusive::list<I3, boost::intrusive::constant_time_size<false>>>("auto_unlink_ilist");
+    bench_fill_back_intrusive<boost::intrusive::list<I3, boost::intrusive::constant_time_size<false>>>("auto_unlink_ilist");*/
     
-    //Result are clear enough with very small size
-    /*if(sizeof(T) == sizeof(long)){
-        graphs::new_graph("fill_front_" + size_str, "fill_front - "  + size_str + " byte", "ms");
-
-        bench_fill_front<std::vector<T>>(fill_front_vector<T>, "vector");
-        bench_fill_front<std::list<T>>(fill_front_list<T>, "list");
-        bench_fill_front<std::deque<T>>(fill_front_deque<T>, "deque");
-    }
-    
-    graphs::new_graph("linear_search_" + size_str, "linear_search - "  + size_str + " byte", "us");
+    /*graphs::new_graph("linear_search_" + size_str, "linear_search - "  + size_str + " byte", "us");
     
     bench_find<std::vector<T>>("vector");
     bench_find<std::list<T>>("list");
-    bench_find<std::deque<T>>("deque");
+    bench_find<std::deque<T>>("deque");*/
     
-    graphs::new_graph("random_insert_" + size_str, "random_insert - "  + size_str + " byte", "ms");
+    /*graphs::new_graph("random_insert_" + size_str, "random_insert - "  + size_str + " byte", "ms");
     
     bench_insert<std::vector<T>>("vector");
     bench_insert<std::list<T>>("list");
-    bench_insert<std::deque<T>>("deque");
+    bench_insert<std::deque<T>>("deque");*/
     
-    graphs::new_graph("random_remove_" + size_str, "random_remove - "  + size_str + " byte", "ms");
+    /*graphs::new_graph("random_remove_" + size_str, "random_remove - "  + size_str + " byte", "ms");
     
     bench_remove<std::vector<T>>("vector");
     bench_remove<std::list<T>>("list");
-    bench_remove<std::deque<T>>("deque");
+    bench_remove<std::deque<T>>("deque");*/
     
-    graphs::new_graph("sort_" + size_str, "sort - "  + size_str + " byte", "ms");
+    graphs::new_graph("write_" + size_str, "write - "  + size_str + " byte", "ms");
 
-    bench_sort<std::vector<T>>(sort_vector<T>, "vector");
-    bench_sort<std::list<T>>(sort_list<T>, "list");
-    bench_sort<std::deque<T>>(sort_deque<T>, "deque");
+    bench_write<std::list<T>>("list");
+    bench_write<boost::intrusive::list<I1, boost::intrusive::constant_time_size<false>>>("normal_ilist");
+    bench_write<boost::intrusive::list<I2, boost::intrusive::constant_time_size<false>>>("safe_ilist");
+    bench_write<boost::intrusive::list<I3, boost::intrusive::constant_time_size<false>>>("auto_unlink_ilist");
     
-    graphs::new_graph("destruction_" + size_str, "destruction - "  + size_str + " byte", "us");
+    /*graphs::new_graph("reverse_" + size_str, "reverse - "  + size_str + " byte", "ms");
+
+    bench_reverse<std::list<T>>("list");
+    bench_reverse<boost::intrusive::list<I1, boost::intrusive::constant_time_size<false>>>("normal_ilist");
+    bench_reverse<boost::intrusive::list<I2, boost::intrusive::constant_time_size<false>>>("safe_ilist");
+    bench_reverse<boost::intrusive::list<I3, boost::intrusive::constant_time_size<false>>>("auto_unlink_ilist");*/
+    
+   /* graphs::new_graph("sort_" + size_str, "sort - "  + size_str + " byte", "ms");
+
+    bench_sort<std::list<T>>("list");
+    bench_sort<boost::intrusive::list<I1, boost::intrusive::constant_time_size<false>>>("normal_ilist");
+    bench_sort<boost::intrusive::list<I2, boost::intrusive::constant_time_size<false>>>("safe_ilist");
+    bench_sort<boost::intrusive::list<I3, boost::intrusive::constant_time_size<false>>>("auto_unlink_ilist");*/
+    
+    /*graphs::new_graph("destruction_" + size_str, "destruction - "  + size_str + " byte", "us");
     
     bench_destruction<std::vector<T>>("vector");
     bench_destruction<std::list<T>>("list");
@@ -502,14 +484,14 @@ void bench(){
 } //end of anonymous namespace
 
 int main(){
-    bench<0>();
-    /*bench<Medium>();
-    bench<Large>();
-    bench<Huge>();
-    bench<Monster>();
-    bench<NonTrivial>();
+    bench<0>();         //8 bytes
+    bench<3>();         //32 bytes
+    bench<15>();        //128 bytes  
+    bench<127>();       //1024 bytes
+    bench<511>();       //4096 bytes
+    //bench<NonTrivial>();
     
-    graphs::new_graph("number_crunching", "number_crunching", "ms");
+    /*graphs::new_graph("number_crunching", "number_crunching", "ms");
 
     bench_number_crunching<std::vector<Small>>("vector");
     bench_number_crunching<std::list<Small>>("list");
